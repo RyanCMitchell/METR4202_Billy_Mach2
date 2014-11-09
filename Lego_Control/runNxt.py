472aafa83a6ad4381cf4aa8ea8545952ba4d8f05
Ryan <ryancallmitch@gmail.com>
2014-11-09 18:02:42 +1000

    Implemented normalised velocities, some other cool small things done too :-) Working on the coordination alignment. Upgraded to air coil with pipe valve & need to find a U-Bend to the final connection.

--- Lego_Control/runNxt.py
+++ Lego_Control/runNxt.py
@@ -20,6 +20,8 @@ def initNXT():
 def runNxt(a,b,c,power,mx,my,mz):
     #create a list of instructions
     instructions = instructionsmake(a,b,c,power)
+    for i in instructions:
+        print i
 
     #get tach readings before running
     x0 = mx.get_tacho()
@@ -50,19 +52,13 @@ def runNxt(a,b,c,power,mx,my,mz):
     for i in instructions:
         runinstruction(i[1:])
 
-    print "mx Tacho: ", mx.get_tacho().tacho_count
-    time.sleep(0.1)
-    print mx._get_new_state().mode
-    time.sleep(0.5)
-    print mx._get_new_state().mode
+    #time.sleep(0.1)
     #query until access is given then read tachos
-
-    A = []; B = []; C = [];
-    withinLimit = False
-    tachoLimit = 2
-    x1 = mx.get_tacho().tacho_count
-    y1 = my.get_tacho().tacho_count
-    z1 = mz.get_tacho().tacho_count
+    while (not(mx.isFinished) or not(my.isFinished) or not(mz.isFinished)):
+        pass
+    #print "Would have gone on"
+    #time.sleep(0.1)
+    
     """
     while withinLimit == False:
         if x1<>x0.tacho_count or y1<>y0.tacho_count or z1<>z0.tacho_count:
@@ -164,7 +160,7 @@ def mainMotorLoop(mx,my,mz):
         #print "Desired", Ad*5,Bd*5,Cd*5
         #print "Actual ", Aa,Ba,Ca
         #print "Error  ", Ea,Eb,Ec
-        if((samePosCount < 10)and flag):
+        if((samePosCount < 5)and flag):
             runNxt(Ea,Eb,Ec,power,mx,my,mz)
 
 def setDesired(x0,y0,z0,power=False):
@@ -185,13 +181,14 @@ def transformWorldToBilly(x,y,z):
 
 def instructionsmake(degx,degy,degz,power):
     if(power==False):
-        
-        time = 1
-        if abs(degx) < 52.5:
+        time = 1.5
+        minSpeed = 30
+        cutOff = minSpeed*time
+        if abs(degx) < cutOff:
             if degx < 0:
-                speed1 = -25
+                speed1 = -minSpeed
             else:
-                speed1 = 25
+                speed1 = minSpeed
         elif abs(degx) > 99:
             if degx < 0:
                 speed1 = -99
@@ -200,11 +197,11 @@ def instructionsmake(degx,degy,degz,power):
         else:
             speed1 = int(degx/time)
             
-        if abs(degy) < 52.5:
+        if abs(degy) < cutOff:
             if degy < 0:
-                speed2 = -25
+                speed2 = -minSpeed
             else:
-                speed2 = 25
+                speed2 = minSpeed
         elif abs(degy) > 99:
             if degy < 0:
                 speed2 = -99
@@ -213,11 +210,11 @@ def instructionsmake(degx,degy,degz,power):
         else:
             speed2 = int(degy/time)
             
-        if abs(degz) < 52.5:
+        if abs(degz) < cutOff:
             if degz < 0:
-                speed3 = -25
+                speed3 = -minSpeed
             else:
-                speed3 = 25
+                speed3 = minSpeed
         elif abs(degz) > 99:
             if degz < 0:
                 speed3 = -99
@@ -239,14 +236,16 @@ def instructionsmake(degx,degy,degz,power):
     else:
         return instructionsmake2(degx,degy,degz,power)
 
+def kinect2Billy(x,y,z):
+    # adjust for left hand rule kinematics (already with corrected z)
 
+    return newx,newy,newz
 
 if __name__=='__main__':
     #dynamixel centre 8.5cm radially from billy centre
     #motor A upper support 32deg to horizontal
     #motor B upper support
     mx,my,mz = initNXT()
-    print dir(mx)
     """
     mx,my,mz = initNXT()
     a = 5.
@@ -280,15 +279,16 @@ if __name__=='__main__':
     t0 = time.time()
     while True:
         t = time.time()-t0
-        rpm = 2
+        rpm = 10
         r = 50
         theta = rpm*(pi/30.)*t
         x_circle = r*cos(theta)
         y_circle = r*sin(theta)
         print "x: ",x_circle," y: ",y_circle
-        setDesired(x_circle+85,y_circle-40,0,100)
-        time.sleep(0.1)
+        setDesired(x_circle+85,y_circle-40,0)
+        time.sleep(0.2)
     """
+    
 
     """
     #step out in x (shit)
@@ -303,46 +303,15 @@ if __name__=='__main__':
         setDesired(x_lin,0,0,50)
         time.sleep(0.1)
     """
+    #(x,y,z) =  kinect2Billy(100,0,0)
+    time.sleep(5)
+    setDesired(100,0,30)
+    time.sleep(5)
+    setDesired(0,0,0)
+    
 
     
-    """time.sleep(3.5)
-    setDesired(150,0,0,80)
-    time.sleep(0.5)
-    setDesired(-50, 20, -10, 80)
-    time.sleep(5)
-    setDesired(0,130,0,80)
-    time.sleep(0.5)
-    setDesired(0,130,0,80)
-    time.sleep(0.5)
-    setDesired(0,-130,0,80)
-    time.sleep(0.5)
-    setDesired(0,0,0,80)
-    time.sleep(0.5)
-    setDesired(0,0,100,80)
-    time.sleep(0.5)
-    setDesired(-130,0,100,80)
-    time.sleep(0.5)
-    setDesired(-130,0,0,80)
-    time.sleep(0.5)
-    setDesired(0,0,100,80)
-    time.sleep(0.5)
-    setDesired(0,0,0,80)
-    time.sleep(0.5)
-    setDesired(100,0,0,80)
-    time.sleep(0.5)
-    setDesired(100,0,50,80)
-    time.sleep(0.5)
-    setDesired(0,0,50,80)
-    time.sleep(0.5)
-    setDesired(0,0,0,80)"""
-
-    #time.sleep(3.5)
-    #setDesired(150,0,0)
-    #time.sleep(1)
-    #setDesired(70, -20, -10)
-    #time.sleep(1)
-
-
+    # raw input value testing
     """while(1):
         move = raw_input("x y z?: ")
         move = move.split(' ')
@@ -352,22 +321,8 @@ if __name__=='__main__':
         time.sleep(2)"""
         
  
-
-    time.sleep(5)
-    setDesired(-130, 0, 0)
-    time.sleep(1)
-    print mx._get_new_state().mode
-    time.sleep(1)
-    print mx._get_new_state().mode
-    time.sleep(0.5)
-    print mx._get_new_state().mode
-    time.sleep(1)
-    print mx._get_new_state().mode
-    time.sleep(1)
-    print mx._get_new_state().mode
-    setDesired(0, 0, 0)
-    time.sleep(5)
-    setDesired(130, 0, 0)
-    time.sleep(5)
-    setDesired(0, 0, 0)
+    #time.sleep(10)
+    #setDesired(130, 0, 0)
+    #time.sleep(10)
+    #setDesired(0, 0, 0)
     
